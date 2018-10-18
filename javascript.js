827426d75780fa21dfefb84162c085b22dfe79f3
Navjot Singh <singhnavjot025@gmail.com>
2018-10-18 23:59:03 +0530

    layout ready

---
+++ javascript.js
@@ -0,0 +1,16 @@
+$(function(){
+    $("#slider").slider({
+        min:3,
+        max:30,
+        slide:function(event,ui){
+            $("#circle").height(ui.value);
+            $("#circle").width(ui.value);
+            
+        }
+    });
+    
+    
+    
+    
+    
+})
